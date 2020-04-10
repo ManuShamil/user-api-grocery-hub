c1496e30519678e8000ea8bd2f4e96c55cb3baa8
Manu Shamil <manusshamil@gmail.com>
2020-04-10 17:49:43 +0530

    Fix for findOneAndUpdate(), changed Static Port

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require('./src/app')
 const { DB_URI } = require("./src/config");
 const mongoose = require("mongoose");
-mongoose.connect(DB_URI);
 
-app.listen(3000, () => {
-    console.log("User-API running on PORT: 3000")
+mongoose.connect(DB_URI,{'useCreateIndex': true})
+
+const PORT = process.env.DEV_PORT || process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`User-API running on PORT: ${PORT}`)
     console.log("-------------------------------")
 })
